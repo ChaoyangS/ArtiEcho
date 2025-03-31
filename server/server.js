e350709ac9681aeaf18d821ca1290e957eb083e9
Chaoyang Shen <chaoyangshen119@gmail.com>
2025-03-31 18:16:59 -0400

    added server

--- server/server.js
+++ server/server.js
@@ -4,17 +4,29 @@
 // const app = express();
 
 // Updated on Mar. 26 for client/public as backend for '/topten-artworks' page
-import express from "express";
-import cors from "cors";
-import artworkRoutes from './routes/artworks.js';
+// import express from "express";
+const express = require("express");
+const cors = require("cors");
+const config = require("./config");
+const routes = require("./routes");
+// import cors from "cors";
+// import artworkRoutes from "./routes/artworks.js";
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/topten-artworks', artworkRoutes);
+// app.use("/topten-artworks", artworkRoutes);
+app.get("/artwork", routes.artwork);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
