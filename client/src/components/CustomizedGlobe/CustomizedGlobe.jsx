a4d652938e87d8d153e67d3230b7e770d01d3de8
Chaoyang Shen <chaoyangshen119@gmail.com>
2025-04-04 15:45:36 -0400

    changed url resolution to 600 600

--- client/src/components/CustomizedGlobe/CustomizedGlobe.jsx
+++ client/src/components/CustomizedGlobe/CustomizedGlobe.jsx
@@ -105,7 +105,7 @@ function CustomizedGlobe() {
             artist: item.preferreddisplayname,
             museum: "Unknown", // I'll add this later
             year: item.beginyear,
-            image: item.url,
+            image: item.url ? item.url.replace("!200,200", "!600,600") : null, //changed to 600*600
           });
         });
 
