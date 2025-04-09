6aac696bc2095ced254f0f76a1b09b071535c250
Chaoyang Shen <chaoyangshen119@gmail.com>
2025-04-09 18:06:35 -0400

    added import for searchpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,23 +8,29 @@ import DonarsPage from "./pages/DonarsPage/DonarsPage";
 import ToptenArtworkByGenrePage from "./pages/ToptenArtworkByGenrePage/ToptenArtworkByGenrePage";
 import Navbar from "./components/Navbar/Navbar";
 import ChatButton from "./components/ChatButton/ChatButton";
+import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
-
   const [count, setCount] = useState(0);
   // Only show Navbar on homepage
-  const showNavbar = location.pathname === "/"; 
+  const showNavbar = location.pathname === "/";
 
   return (
     <>
-    {showNavbar && <Navbar />}
-    {/*<h1>ArtiEcho</h1>*/}
+      {showNavbar && <Navbar />}
+      {/*<h1>ArtiEcho</h1>*/}
       {/* Routes for different pages */}
       <Routes>
         <Route path="/" element={<CustomizedGlobe />} /> {/* Homepage route */}
-        <Route path="/topten-artworks" element={<ToptenArtworkPage />} /> {/* Top 10 Artworks by style-genre page route */}
-        <Route path="/topten-artworks-by-genre" element={<ToptenArtworkByGenrePage/>}/> {/* Top 10 Artworks by genre-style page route */}
-        <Route path="/topten-artists" element={<ToptenArtworkPage />} /> {/* Top 10 Artists by number of artworks */}
+        <Route path="/topten-artworks" element={<ToptenArtworkPage />} />{" "}
+        {/* Top 10 Artworks by style-genre page route */}
+        <Route
+          path="/topten-artworks-by-genre"
+          element={<ToptenArtworkByGenrePage />}
+        />{" "}
+        {/* Top 10 Artworks by genre-style page route */}
+        <Route path="/topten-artists" element={<ToptenArtworkPage />} />{" "}
+        {/* Top 10 Artists by number of artworks */}
         <Route path="/donars" element={<DonarsPage />} />
         <Route path="search" element={<SearchPage />} />
       </Routes>
