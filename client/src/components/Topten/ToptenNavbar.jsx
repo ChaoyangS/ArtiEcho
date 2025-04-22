915b217a1342d2508df425856553acc7f2e67b9a
Vivian Gao <vivian@Mac.attlocal.net>
2025-04-21 22:51:18 -0700

    Topten: fix sidebar hint for user and overflow title

--- client/src/components/Topten/ToptenNavbar.jsx
+++ client/src/components/Topten/ToptenNavbar.jsx
@@ -59,7 +59,7 @@ const ToptenNavbar = (props) => {
         ref={sidebarRef}
       >
         
-      <div className="sidebar-title" onClick={() => setSidebarOpen(!sidebarOpen)}>
+        <div className="sidebar-title" onClick={() => setSidebarOpen(!sidebarOpen)}>
         <svg
           viewBox="0 0 24 24"
           fill="none"
@@ -73,6 +73,12 @@ const ToptenNavbar = (props) => {
           <line x1="3" y1="12" x2="21" y2="12" />
           <line x1="3" y1="18" x2="21" y2="18" />
         </svg>
+        {!sidebarOpen && (
+          <div className="sidebar-still">
+            <p> &nbsp; &nbsp; &nbsp; Find&nbsp;  your&nbsp;  artistic&nbsp;  match&nbsp; &nbsp; &nbsp; &nbsp; &#8675;&#8675;&#8675;</p>
+          </div>
+        )}
+        
       </div>
 
         <div className="hover-sidebar-inner">
