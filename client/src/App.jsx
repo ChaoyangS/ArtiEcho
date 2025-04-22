96aa68ea9c3be707f80f20c7ae242fcc4625adcd
Chaoyang Shen <chaoyangshen119@gmail.com>
2025-04-22 12:00:03 -0400

    cleaned up folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import "./styles/_global.css";
-import CustomizedGlobe from "./components/CustomizedGlobe/CustomizedGlobe";
 import ToptenArtworkPage from "./pages/ToptenArtworkPage/ToptenArtworkPage";
 import DonarsPage from "./pages/DonarsPage/DonarsPage";
 import ToptenArtworkByGenrePage from "./pages/ToptenArtworkByGenrePage/ToptenArtworkByGenrePage";
